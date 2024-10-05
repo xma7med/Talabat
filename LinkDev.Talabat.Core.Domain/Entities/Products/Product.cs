5df183ec29f244df9454bab7b4c62fbe8f7ea15a
Mohamed Nasser <xma7med@gmail.com>
2024-10-05 21:43:38 +0300

    Part 03 Product Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Products/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Products
+{
+	public class Product
+	{
+        public required string  Name { get; set; }
+        public required string Description { get; set; }
+        public string? PictureUrl { get; set; }
+        public decimal Price { get; set; }
+
+        
+        public int? BrandId { get; set; } // Foriegn Key --> ProductBrand 
+        public virtual ProductBrand? Brand { get; set; }
+
+        public int? CategoryId { get; set; } // Foriegn Key --> ProductCategory 
+		public virtual ProductCategory? Category { get; set; }
+    }
+}
---
+++ LinkDev.Talabat.Core.Domain/Entities/Products/ProductBrand.cs
@@ -0,0 +1,19 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Products
+{
+	public class ProductBrand : BaseEntity<int>
+	{
+        public required string  Name { get; set; }
+
+
+    }
+}
+
+        // i dont need to navigate from here + there are infinty loop prop here 
+        //public ICollection<Product> Products { get; set; } = new HashSet<Product>();
