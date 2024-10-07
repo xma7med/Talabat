1f5e308c6c9f927585e8095c8ad25673ce23a72f
Mohamed Nasser <xma7med@gmail.com>
2024-10-07 21:41:12 +0300

    Part 10 Unit Of Work [Final Implementation]

--- LinkDev.Talabat.Core.Domain/Contracts/IUnitOfWork.cs
+++ LinkDev.Talabat.Core.Domain/Contracts/IUnitOfWork.cs
@@ -9,11 +9,18 @@ namespace LinkDev.Talabat.Core.Domain.Contracts
 {
 	public interface IUnitOfWork:IAsyncDisposable
 	{
-		public IGenericRepository<Product, int> ProductRepository { get; set; }
-		public IGenericRepository<ProductBrand, int> BrandsRepository { get; set; }
-		public IGenericRepository<ProductCategory, int> CategoriesRepository { get; set; }
-
+		
+		IGenericRepository<TEntity, Tkey> GetRepository<TEntity, Tkey>()
+			where TEntity : BaseEntity<Tkey> where Tkey : IEquatable<Tkey>;
+			
 		Task<int> CompleteAsync();
+		#region 3rd way using Lazy ---
+		//public IGenericRepository<Product, int> ProductRepository { get; set; }
+		//public IGenericRepository<ProductBrand, int> BrandsRepository { get; set; }
+		//public IGenericRepository<ProductCategory, int> CategoriesRepository { get; set; }
+
+		//Task<int> CompleteAsync(); 
+		#endregion
 
 	}
 }
--- LinkDev.Talabat.Infrastructure.Presistance/UnitOfWork/UnitOfWork.cs
+++ LinkDev.Talabat.Infrastructure.Presistance/UnitOfWork/UnitOfWork.cs
@@ -1,44 +1,82 @@
-﻿using LinkDev.Talabat.Core.Domain.Contracts;
-using LinkDev.Talabat.Core.Domain.Entities.Products;
+﻿using LinkDev.Talabat.Core.Domain.Common;
+using LinkDev.Talabat.Core.Domain.Contracts;
 using LinkDev.Talabat.Infrastructure.Presistance.Data;
 using LinkDev.Talabat.Infrastructure.Presistance.Reositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace LinkDev.Talabat.Infrastructure.Presistance.UnitOfWork
 {
-	public  class UnitOfWork : IUnitOfWork
+	public class UnitOfWork : IUnitOfWork
 	{
 		private readonly StoreContext _dbContext;
 
-		private readonly Lazy<IGenericRepository<Product, int> >_ProductRepository;
-		private readonly Lazy<IGenericRepository<ProductBrand, int> >_brandRepository;
-		private readonly Lazy<IGenericRepository<ProductCategory, int> > _categoryRepository;
+		private readonly ConcurrentDictionary<string, object> _repositories;
+		// Common way to implement Unit Of Work 
 
-		public UnitOfWork(StoreContext  dbContext)
+		public UnitOfWork(StoreContext dbContext)
         {
 			_dbContext = dbContext;
-			_ProductRepository = new Lazy<IGenericRepository<Product, int>>(() => new GenaricRepository<Product,int>(_dbContext));
-			_brandRepository = new Lazy<IGenericRepository<ProductBrand, int>>(()=> new GenaricRepository<ProductBrand, int>(_dbContext));
-			_categoryRepository = new Lazy<IGenericRepository<ProductCategory, int>>(() => new GenaricRepository<ProductCategory, int>(_dbContext));
+			               // syntax suger .net  c# 9
+			_repositories = new /*ConcurrentDictionary<string, object>*/();
 		}
-		public IGenericRepository<Product, int> ProductRepository => _ProductRepository.Value;/*new GenaricRepository<Product, int>(_dbContext);*/
-		public IGenericRepository<ProductBrand, int> BrandsRepository => _brandRepository.Value; /*new GenaricRepository<ProductBrand, int>(_dbContext);*/
-		public IGenericRepository<ProductCategory, int> CategoriesRepository => _categoryRepository.Value;/*new GenaricRepository<ProductCategory, int>(_dbContext);*/
 
-
-
-		public Task<int> CompleteAsync()
+        public IGenericRepository<TEntity, Tkey> GetRepository<TEntity, Tkey>()
+			where TEntity : BaseEntity<Tkey>
+			where Tkey : IEquatable<Tkey>
 		{
-			throw new NotImplementedException();
-		}
+			///1- هتبقى مشكله لو نادى ع الميثود اكتر من مره بنفس التايب هيعمله اكتر من اوبجكت
+			///return new GenaricRepository<TEntity,Tkey>(_dbContext);	
 
-		public ValueTask DisposeAsync()
-		{
-			throw new NotImplementedException();
+			///2- with Asyncrounas programming we use ConcurrentCollections
+			///var typeName = typeof(TEntity).Name; // For Ex : Product
+		    ///if (_repositories.ContainsKey(typeName)) return (IGenericRepository<TEntity, Tkey>) _repositories[typeName];
+			///
+			///var repository = new GenaricRepository<TEntity,Tkey>(_dbContext);
+			///_repositories.Add(typeName, repository);
+			///
+			///return repository;
+
+			return (IGenericRepository<TEntity, Tkey>)_repositories.GetOrAdd(typeof(TEntity).Name , new GenaricRepository<TEntity,Tkey> (_dbContext));
+
 		}
+		public Task<int> CompleteAsync()=> _dbContext.SaveChangesAsync();	
+
+		public async ValueTask DisposeAsync()=> await _dbContext.DisposeAsync();	
+		
+		
+		
+		
+		
+		#region 3rd Way to implement Unit Of Work using Lazy Initialization  
+		//private readonly StoreContext _dbContext;
+
+		//private readonly Lazy<IGenericRepository<Product, int> >_ProductRepository;
+		//private readonly Lazy<IGenericRepository<ProductBrand, int> >_brandRepository;
+		//private readonly Lazy<IGenericRepository<ProductCategory, int> > _categoryRepository;
+
+		//public UnitOfWork(StoreContext  dbContext)
+		//      {
+		//	_dbContext = dbContext;
+		//	_ProductRepository = new Lazy<IGenericRepository<Product, int>>(() => new GenaricRepository<Product,int>(_dbContext));
+		//	_brandRepository = new Lazy<IGenericRepository<ProductBrand, int>>(()=> new GenaricRepository<ProductBrand, int>(_dbContext));
+		//	_categoryRepository = new Lazy<IGenericRepository<ProductCategory, int>>(() => new GenaricRepository<ProductCategory, int>(_dbContext));
+		//}
+		//public IGenericRepository<Product, int> ProductRepository => _ProductRepository.Value;/*new GenaricRepository<Product, int>(_dbContext);*/
+		//public IGenericRepository<ProductBrand, int> BrandsRepository => _brandRepository.Value; /*new GenaricRepository<ProductBrand, int>(_dbContext);*/
+		//public IGenericRepository<ProductCategory, int> CategoriesRepository => _categoryRepository.Value;/*new GenaricRepository<ProductCategory, int>(_dbContext);*/
+
+
+
+		//public Task<int> CompleteAsync()
+		//{
+		//	throw new NotImplementedException();
+		//}
+
+		//public ValueTask DisposeAsync()
+		//{
+		//	throw new NotImplementedException();
+		//} 
+		#endregion
+
 	}
 }
