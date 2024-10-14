498b49fd339818c785617241090c41fe9f281415
Mohamed Nasser <xma7med@gmail.com>
2024-10-14 15:39:48 +0300

    Conflict Problem

--- LinkDev.Talabat.Core.Application/Services/Products/ProducService.cs
+++ LinkDev.Talabat.Core.Application/Services/Products/ProducService.cs
@@ -11,49 +11,49 @@ namespace LinkDev.Talabat.Core.Application.Services.Products
 	// Remember I dont Neeed It PUBLIC OutSide layer dependOn ABSTRACTION NOT IMPLEMENTATION 
 	// INJECT MAPPER  - Allow DI for IMapper 
 	internal class ProducService(IUnitOfWork unitofWork, IMapper mapper) : IProductService
-    {
+	{
 		//                                                            specParams= sort, brandId , categoryId
 		public async Task<Pagination<ProductToReturnDto>> GetProductsAsync(ProductSpecParams specParams)
-        {
-            /// Prop : non genaric  & i cant erite query here End point best practice should be less lines 
+		{
+			/// Prop : non genaric  & i cant erite query here End point best practice should be less lines 
 			/// var specs = new BaseSpecifications<Product, int>(); 
-            ///specs.Includes.Add(P=>P.Brand)!;
-            ///specs.Includes.Add(P=>P.Category)!; 
-            var specs = new ProductWithBrandAndCategorySpecifications(specParams.Sort, specParams.BrandId , specParams.CategoryId ,specParams.PageSize , specParams.PageIndex , specParams.Search );
-            var /*products*/data =mapper.Map<IEnumerable<ProductToReturnDto>>(await unitofWork.GetRepository<Product, int>().GetAllWithSpecAsync(specs));
+			///specs.Includes.Add(P=>P.Brand)!;
+			///specs.Includes.Add(P=>P.Category)!; 
+			var specs = new ProductWithBrandAndCategorySpecifications(specParams.Sort, specParams.BrandId, specParams.CategoryId, specParams.PageSize, specParams.PageIndex, specParams.Search);
+			var /*products*/data = mapper.Map<IEnumerable<ProductToReturnDto>>(await unitofWork.GetRepository<Product, int>().GetAllWithSpecAsync(specs));
 			//return products;
 
 			var countSpec = new ProductWithFilterationForCountSpecifications(specParams.BrandId, specParams.CategoryId, specParams.Search);
-			var count = await unitofWork.GetRepository<Product,int>().GetCountAsync(countSpec);	
-			return new Pagination<ProductToReturnDto>(specParams.PageSize, specParams.PageIndex, count  ) { Data = data };
+			var count = await unitofWork.GetRepository<Product, int>().GetCountAsync(countSpec);
+			return new Pagination<ProductToReturnDto>(specParams.PageSize, specParams.PageIndex, count) { Data = data };
 		}
 		//=> mapper.Map<IEnumerable<ProductToReturnDto>>(await unitofWork.GetRepository<Product, int>().GetAllAsync());
 
 		public async Task<ProductToReturnDto> GetProductAsync(int id)
-        {
+		{
 			var specs = new ProductWithBrandAndCategorySpecifications(id);  // Creating specifications object with the product id
 			var product = await unitofWork.GetRepository<Product, int>().GetWithSpecAsync(specs);  // Using the specification to get the product asynchronously
 			var mappedProduct = mapper.Map<ProductToReturnDto>(product);  // Mapping the product to a DTO (ProductToReturnDto)
 
-			return mappedProduct;  
+			return mappedProduct;
 
 		}
 
 
-        // you can make Enhancement -- Lookup(Search)
-        public async Task<IEnumerable<BrandDto>> GetBrandsAsync()
-        {
+		// you can make Enhancement -- Lookup(Search)
+		public async Task<IEnumerable<BrandDto>> GetBrandsAsync()
+		{
 			var brands = await unitofWork.GetRepository<ProductBrand, int>().GetAllAsync();
 			var brandsToReturn = mapper.Map<IEnumerable<BrandDto>>(brands);
 
 			return brandsToReturn;
 		}
-            //=> mapper.Map<IEnumerable<BrandDto>>(await unitofWork.GetRepository<ProductBrand, int>().GetAllAsync());
+		//=> mapper.Map<IEnumerable<BrandDto>>(await unitofWork.GetRepository<ProductBrand, int>().GetAllAsync());
 
 
-        public async Task<IEnumerable<CategoryDto>> GetcategoriesAsync()
-          => mapper.Map<IEnumerable<CategoryDto>>(await unitofWork.GetRepository<ProductCategory, int>().GetAllAsync());
+		public async Task<IEnumerable<CategoryDto>> GetcategoriesAsync()
+		  => mapper.Map<IEnumerable<CategoryDto>>(await unitofWork.GetRepository<ProductCategory, int>().GetAllAsync());
 
 
-    }
+	}
 }
--- LinkDev.Talabat.Infrastructure.Presistance/Reositories/Generic Repository/GenaricRepository.cs
+++ LinkDev.Talabat.Infrastructure.Presistance/Reositories/Generic Repository/GenaricRepository.cs
@@ -13,18 +13,18 @@ namespace LinkDev.Talabat.Infrastructure.Presistance.Reositories.Generic_Reposit
 		public async Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking = false)
 		//=> withTracking ? await DbContext.Set<TEntity>().ToListAsync() : await DbContext.Set<TEntity>().AsNoTracking().ToListAsync();
 		{
-		// ******** Fixed --> but da mosakn l2n msh by722 el [open for extention close for modification]
+			// ******** Fixed --> but da mosakn l2n msh by722 el [open for extention close for modification]
 			/// will use specification Design pattern to achieve ==> [open for extention close for modification]
 			///if (typeof(TEntity) == typeof(Product)) 
 			///{
 			///	return withTracking? (IEnumerable<TEntity>) (await _dbContext.Set<Product>().Include(P => P.Brand).Include(P => P.Category).ToListAsync() ):
 			///		(IEnumerable<TEntity>)(await _dbContext.Set<Product>().Include(P => P.Brand).Include(P => P.Category).AsNoTracking().ToListAsync());
 			///}
-			 
+
 			///  for other entities Brand - Category ...
-			return withTracking ? 
+			return withTracking ?
 				await _dbContext.Set<TEntity>().ToListAsync() :
-				await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync(); 
+				await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync();
 
 		}
 		///{
@@ -32,26 +32,26 @@ namespace LinkDev.Talabat.Infrastructure.Presistance.Reositories.Generic_Reposit
 		///		return await  _dbContext.Set<TEntity>().ToListAsync();	
 		///	 return await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync();
 		///}
-		public async Task<IEnumerable<TEntity>> GetAllWithSpecAsync (ISpecifications<TEntity, TKey> spec, bool withTracking = false)
+		public async Task<IEnumerable<TEntity>> GetAllWithSpecAsync(ISpecifications<TEntity, TKey> spec, bool withTracking = false)
 		{//       -------------------------Qurery------------------------------------------------ .ToListAsync()
-			return await  /*SpecificationEvaluator<TEntity, TKey>.GetQuery(DbContext.Set<TEntity>() , spec)*/ApplySpecifications( spec).ToListAsync();
+			return await  /*SpecificationEvaluator<TEntity, TKey>.GetQuery(DbContext.Set<TEntity>() , spec)*/ApplySpecifications(spec).ToListAsync();
 		}
 		public async Task<TEntity?> GetWithSpecAsync(ISpecifications<TEntity, TKey> spec)
 		{
-			return await ApplySpecifications( spec).FirstOrDefaultAsync();		
+			return await ApplySpecifications(spec).FirstOrDefaultAsync();
 		}
 
 
 		// To Get Count 
 		public async Task<int> GetCountAsync(ISpecifications<TEntity, TKey> spec)
 		{
-			return await ApplySpecifications(spec).CountAsync();	
+			return await ApplySpecifications(spec).CountAsync();
 		}
 
 
 		public async Task<TEntity?> GetAsync(TKey id)  /*=> await DbContext.Set<TEntity>().FindAsync(id);*/ //1
 		{
-		// ********Fixed but this not Consider O in Solid The sol is Specification Design Pattern 
+			// ********Fixed but this not Consider O in Solid The sol is Specification Design Pattern 
 			/// will use specification Design pattern to achieve ==> [open for extention close for modification]
 			///if (typeof(TEntity) == typeof(Product))
 			///	// Return a product with its Brand and Category eagerly loaded
@@ -70,21 +70,21 @@ namespace LinkDev.Talabat.Infrastructure.Presistance.Reositories.Generic_Reposit
 
 
 		public async Task AddAsync(TEntity entity) => await _dbContext.Set<TEntity>().AddAsync(entity);
-		
-		public void Update(TEntity entity) =>_dbContext.Set<TEntity>().Update(entity);
-		
 
-		public void Delete(TEntity entity) => 	_dbContext.Set<TEntity>().Remove(entity);
+		public void Update(TEntity entity) => _dbContext.Set<TEntity>().Update(entity);
+
+
+		public void Delete(TEntity entity) => _dbContext.Set<TEntity>().Remove(entity);
 
 
 		#region Helpers
 
-		private  IQueryable<TEntity> ApplySpecifications( ISpecifications<TEntity, TKey> spec)
+		private IQueryable<TEntity> ApplySpecifications(ISpecifications<TEntity, TKey> spec)
 		{
 			return SpecificationEvaluator<TEntity, TKey>.GetQuery(_dbContext.Set<TEntity>(), spec);
 		}
 
-		
+
 
 
 		#endregion
