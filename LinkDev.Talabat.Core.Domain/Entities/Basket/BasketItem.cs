f296680a626131c61578aec5f07aad19c6270d4d
Mohamed Nasser <xma7med@gmail.com>
2024-10-15 14:37:44 +0300

    Part 10 Basket Module Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Basket/BasketItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Basket
+{
+	public class BasketItem
+	{
+		public int Id { get; set; } // Will be the same product Id
+		public required string ProductName { get; set; }
+		public string? PictureUrl { get; set; }
+		public decimal Price { get; set; }
+		public int Quantity { get; set; }
+		public string? Brand { get; set; }
+		public string? Category { get; set; }
+
+	}
+}
---
+++ LinkDev.Talabat.Core.Domain/Entities/Basket/CustomerBasket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Basket
+{
+	public class CustomerBasket
+	{
+		public required string Id { get; set; }
+		public IEnumerable<BasketItem> Items { get; set; } = new List<BasketItem>();	
+
+	}
+}
