1f0574312f25423969b31885c5a60c3f7852c3e7
Mohamed Nasser <xma7med@gmail.com>
2024-10-17 20:42:44 +0300

    Part 01 Identity Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Identity/Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Identity
+{
+	public class Address
+	{
+		public int Id { get; set; }
+		public required string FirstName { get; set; }
+		public required string LastName { get; set; }
+		public required string Street { get; set; }
+		public required string City { get; set; }
+		public required string Country { get; set; }
+        public int UserId { get; set; }
+
+        /// Any Relation by default mapped 1 to many 
+        /// Addede FK in the Required Side  
+        /// Must Add it And make Data Annotation or fluent API to make it unique (No Duplicate )
+
+        public required ApplicationUser AppUser { get; set; } // Mandatory 
+
+    }
+}
---
+++ LinkDev.Talabat.Core.Domain/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Identity
+{
+	public class ApplicationUser
+	{
+        public required string DisplayName { get; set; }
+        public Address? Address  { get; set; } // Optional
+    }
+}
