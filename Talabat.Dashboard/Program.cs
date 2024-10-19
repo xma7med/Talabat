3f88f1e2d3158275cf04624fc31df470feddfed3
Mohamed Nasser <xma7med@gmail.com>
2024-10-19 20:13:31 +0300

    Part 3 - User  Controller (Edit )

--- Talabat.Dashboard/Program.cs
+++ Talabat.Dashboard/Program.cs
@@ -3,53 +3,30 @@ using LinkDev.Talabat.Infrastructure.Presistance.Data;
 using LinkDev.Talabat.Infrastructure.Presistance.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using StackExchange.Redis;
 
 namespace Talabat.Dashboard
 {
-	public class Program
+    public class Program
 	{
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
-
-
 			// Add services to the container.
 			// first step bring all configs related to DB 
-			#region For DB
 			builder.Services.AddControllersWithViews();
+			#region For DB
 			builder.Services.AddDbContext<StoreDbContext>((optionBuilder) =>
 			{
 				optionBuilder.UseLazyLoadingProxies()
 				.UseSqlServer(builder.Configuration.GetConnectionString("StoreContext"));
-			} /*, contextLifetime: ServiceLifetime.Scoped , optionsLifetime : ServiceLifetime.Scoped*/);
-
+			});
 			// to let him know that i will use appUser insted of IdentityUser
 			builder.Services.AddIdentity<ApplicationUser, IdentityRole>((identityOptions) =>
 			{
-				//identityOptions.SignIn.RequireConfirmedAccount = true;
-				//identityOptions.SignIn.RequireConfirmedEmail = true;
-				//identityOptions.SignIn.RequireConfirmedPhoneNumber = true;
-
-				/// identityOptions.Password.RequireNonAlphanumeric = true; // #$@%
-				/// identityOptions.Password.RequiredUniqueChars = 2;
-				/// identityOptions.Password.RequiredLength = 6;
-				/// identityOptions.Password.RequireDigit = true;
-				/// identityOptions.Password.RequireLowercase = true;
-				/// identityOptions.Password.RequireUppercase = true;
-
 				identityOptions.User.RequireUniqueEmail = true;
-				// identityOptions.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
-
 				identityOptions.Lockout.AllowedForNewUsers = true;
 				identityOptions.Lockout.MaxFailedAccessAttempts = 5;
 				identityOptions.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromHours(12);
-
-				/// Search 
-				//identityOptions.Stores
-				//identityOptions.Tokens
-				//identityOptions.ClaimsIdentity
-
 			})
 			.AddEntityFrameworkStores<StoreIdentityDbContext>();
 
