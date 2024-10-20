f76eb302bc34ff801dfa9cf8e7900440cbaad544
Mohamed Nasser <xma7med@gmail.com>
2024-10-20 15:14:10 +0300

    Part 05 Brand Controller

---
+++ Talabat.Dashboard/Models/ProductBrandViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Talabat.Dashboard.Models
+{
+    public class ProductBrandViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CreatedBy { get; set; } = "1";
+        public string LastModifiedBy { get; set; } = "1";
+    }
+}
