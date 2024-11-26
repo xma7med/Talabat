304103df2a2dbd40e36ead9360ba99ed073704fe
Mohamed Nasser <xma7med@gmail.com>
2024-11-26 18:30:24 +0200

    Part 01 Order Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Orders/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Orders
+{
+    public class OrderItem: BaseAuditableEntity<int>
+    {
+        public required ProductItemOrdered Product { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
