e0aaf3382f1e665e5dd27abe8cd5087e7ce2b6f4
Mohamed Nasser <xma7med@gmail.com>
2025-01-05 21:11:41 +0200

    Register service

--- LinkDev.Talabat.Infrastructure/DependencyInjection.cs
+++ LinkDev.Talabat.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using LinkDev.Talabat.Core.Domain.Contracts.Infrastructure;
 using LinkDev.Talabat.Infrastructure.Basket_Repository;
+using LinkDev.Talabat.Infrastructure.Cach_Service;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using StackExchange.Redis;
@@ -17,7 +18,11 @@ namespace LinkDev.Talabat.Infrastructure
 				return connectionMultiplexerObj;
 			});
 
-			services.AddScoped (typeof(IBasketRepository), typeof(BasketRepository));	
+			// SingleTon Bec i want it to live untill the session is treminated 
+			services.AddSingleton(typeof(IResponseCasheService) , typeof(ResponseCasheService));	
+			services.AddScoped (typeof(IBasketRepository), typeof(BasketRepository));
+
+			//services.Configure<RedisSettings>(configuration.GetSection("RedisSettings"));
 
 			return services;
 		}
