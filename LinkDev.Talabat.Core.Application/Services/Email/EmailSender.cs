70911f13f8b051f20b5ec941218d3467b38513e5
xma7med <xma7med@gmail.com>
2025-06-15 02:38:46 +0300

    Setup for email sender

---
+++ LinkDev.Talabat.Core.Application/Services/Email/EmailSender.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+
+namespace LinkDev.Talabat.Core.Application.Services.Email
+{
+    /// need to configure it read more in app setting .... <summary>
+    /// need to transform AuthMessageSenderOptions class from here 
+    /// Make interface
+    /// register service 
+    /// 
+    /// </summary>
+    public class AuthMessageSenderOptions
+    {
+        public string? SendGridKey { get; set; }
+        public string? SendGridUser { get; set; } // optional
+    }
+
+    public class EmailSender : IEmailSender
+    {
+        private readonly ILogger _logger;
+
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor,
+                           ILogger<EmailSender> logger)
+        {
+            Options = optionsAccessor.Value;
+            _logger = logger;
+        }
+
+        public AuthMessageSenderOptions Options { get; } //Set with Secret Manager.
+
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            if (string.IsNullOrEmpty(Options.SendGridKey))
+            {
+                throw new System.Exception("Null SendGridKey");
+            }
+            await Execute(Options.SendGridKey, subject, message, toEmail);
+        }
+
+        public async Task Execute(string apiKey, string subject, string message, string toEmail)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("Joe@contoso.com", "Password Recovery"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(toEmail));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+            msg.SetClickTracking(false, false);
+            var response = await client.SendEmailAsync(msg);
+            _logger.LogInformation(response.IsSuccessStatusCode
+                                   ? $"Email to {toEmail} queued successfully!"
+                                   : $"Failure Email to {toEmail}");
+        }
+    }
+}
